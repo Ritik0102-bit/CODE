d4d02478b1b84294e5c9aef110e4e082d26031bc
Ritik <Ritikranaagra82@gmail.com>
2025-12-23 12:00:00 +0530

    Add C++ doubly linked list practice files

--- C++/Linked_list_24.cpp
+++ C++/Linked_list_24.cpp
@@ -1,40 +1,51 @@
+// we need to delete array elements from the linked list
+
 // 2 -> 8 -> 3 -> 2 -> 2 -> 9 -> 2 -> NULL
 // arr={3,2}
 
-// we need to delete array elements from the linked list
+// Step 1. Store all the values of nums array in set
+// Step 2. now delete elements from linked list which are present in the set
+
 
 #include<iostream>
+#include<vector>
+#include<unordered_set>
 
 using namespace std;
 
-class Node{
+class ListNode{
     public:
     int val;
-    Node* next;
+    ListNode* next;
 
-    Node(int data){
+    ListNode(int data){
         val=data;
         next=NULL;
     }
+
+    ListNode(int data,ListNode* nextNode){
+        val=data;
+        next=nextNode;
+    }
 };
 
-void insert_at_tail(Node* &head,int value){
-    Node* new_node=new Node(value);
-
+void insert_at_tail(ListNode* &head,int value){
+    ListNode* new_node=new ListNode(value);
+    
     if(head==NULL){
         head=new_node;
         return;
     }
-
-    Node* temp=head;
+    
+    ListNode* temp=head;
     while(temp->next!=NULL){
         temp=temp->next;
     }
     temp->next=new_node;
 }
 
-void print_linked_list(Node* head){
-    Node* temp=head;
+void print_linked_list(ListNode* head){
+    ListNode* temp=head;
     while(temp!=NULL){
         cout << temp->val << " -> ";
         temp=temp->next;
@@ -42,49 +53,54 @@ void print_linked_list(Node* head){
     cout << "NULL" << endl;
 }
 
-void delete_array_elements(Node* &head,int x){
-    Node* temp=head;
-    while(temp->next!=NULL){
-        while(temp->val==x){
-            temp->val=temp->next->val;
-            temp->next=temp->next->next;
-        }
-        temp=temp->next;
+ListNode* modifiedList(vector<int>& nums, ListNode* head){
+    if (head == nullptr){
+        return nullptr;
     }
-    if(temp->val==x){
-        temp=NULL;
-    }
-    return;
-}
 
-void delete_array_elements_2(Node* &head,int x){
-    return;
+    unordered_set<int> s;
+    for (int val : nums){
+        s.insert(val);
+    }
+
+    // Pro-tip 1: Initialize the set in one line
+    // unordered_set<int> s(nums.begin(), nums.end());
+
+    ListNode *DummyNode = new ListNode(0, head);
+    ListNode *temp = DummyNode;
+
+    // Pro-tip 2: Allocate DummyNode on the stack to avoid 'new' and 'delete'
+    // ListNode DummyNode(0, head); 
+    // ListNode* temp = &DummyNode;
+
+    while (temp != nullptr && temp->next != nullptr){
+        if (s.find(temp->next->val) != s.end()){
+            temp->next = temp->next->next;
+        }
+        else{
+            temp = temp->next;
+        }
+    }
+
+    return DummyNode->next;
 }
 
 int main(){
-    Node* linked_list=new Node(2);
+    vector<int> nums = {3,2};
 
-    insert_at_tail(linked_list,3);
-    insert_at_tail(linked_list,3);
-    insert_at_tail(linked_list,8);
-    insert_at_tail(linked_list,3);
-    insert_at_tail(linked_list,2);
-    insert_at_tail(linked_list,2);
-    insert_at_tail(linked_list,9);
-    insert_at_tail(linked_list,2);
+    ListNode* head = new ListNode(2);
+    insert_at_tail(head,3);
+    insert_at_tail(head,3);
+    insert_at_tail(head,8);
+    insert_at_tail(head,3);
+    insert_at_tail(head,2);
+    insert_at_tail(head,2);
+    insert_at_tail(head,9);
+    insert_at_tail(head,2);
 
-    print_linked_list(linked_list);
-    
-    int arr[2]={3,2};
-    
-    for(int i=0;i<2;i++){
-        if(linked_list==NULL){
-            break;
-        }
-        delete_array_elements(linked_list,arr[i]);
-    }
-    
-    print_linked_list(linked_list);
+    head = modifiedList(nums, head);
+
+    print_linked_list(head);
 
     return 0;
 }
