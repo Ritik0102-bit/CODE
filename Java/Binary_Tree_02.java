edc6893afdb0723b75628b5c4d39c8d75269df0d
Ritik <Ritikranaagra82@gmail.com>
2026-01-07 12:00:00 +0530

    Refine Java binary tree practice on two additional examples

--- Java/Binary_Tree_02.java
+++ Java/Binary_Tree_02.java
@@ -1,3 +1,39 @@
+// Height of a Binary Tree
+
 public class Binary_Tree_02 {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
     
+    //Returns height of a binary tree
+    public static int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Height of the tree is "+height(root));
+
+        // Output:
+        // Height of the tree is 3
+    }
 }
--- Java/Binary_Tree_03.java
+++ Java/Binary_Tree_03.java
@@ -1,3 +1,36 @@
+// Count of nodes in a Binary Tree
+
 public class Binary_Tree_03 {
-    
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int count(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        return 1 + count(root.left) + count(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Count of nodes in the tree is "+count(root));
+
+        // Output:
+        // Count of nodes in the tree is 7
+    }
 }
