6d46ec30b6cc501ff2a8c656708f1c9ff344330a
Ritik <Ritikranaagra82@gmail.com>
2026-03-02 12:00:00 +0530

    Update Pascal Triangle implementation (C++)

--- C++/Pascal_Triangle.cpp
+++ C++/Pascal_Triangle.cpp
@@ -1,19 +1,37 @@
 // Pascal's Triangle
 
-vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-        
-        for (int i = 0; i < numRows; i++) {
-            // Create a row of size i + 1, initialized entirely with 1s
-            vector<int> row(i + 1, 1);
-            
-            // Calculate the inner elements (skipping the first and last indices)
-            for (int j = 1; j < i; j++) {
-                row[j] = res[i - 1][j - 1] + res[i - 1][j];
-            }
-            
-            res.push_back(row);
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> PascalTriangle(int numRows){
+    vector<vector<int>> res;
+
+    for (int i = 0; i < numRows; i++){
+        // Create a row of size i + 1, initialized entirely with 1s
+        vector<int> row(i + 1, 1);
+
+        // Calculate the inner elements (skipping the first and last indices)
+        for (int j = 1; j < i; j++){
+            row[j] = res[i - 1][j - 1] + res[i - 1][j];
         }
-        
-        return res;
+
+        res.push_back(row);
     }
+
+    return res;
+}
+
+int main(){
+    vector<vector<int>> ans = PascalTriangle(6);
+
+    for (int i = 0; i < ans.size(); i++){
+        for (int j = 0; j < ans[i].size(); j++){
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
