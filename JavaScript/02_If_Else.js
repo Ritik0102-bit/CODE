0a8a48a98ea58cf77d093ccc6e165222c16d52db
Ritik Rana <Ritikranaagra82@gmail.com>
2026-04-21 22:47:12 +0530

    Add If-Else statement in JavaScript

---
+++ JavaScript/02_If_Else.js
@@ -0,0 +1,11 @@
+let country = "India";
+
+if (country === "India") {
+    console.log("Hello from India");
+} 
+else if (country === "USA") {
+    console.log("Hello from USA");
+} 
+else {
+    console.log("Hello from somewhere else");
+}
