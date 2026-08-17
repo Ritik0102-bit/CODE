a8000be5b986be3f188eef7853d86bb41e332968
Ritik <Ritikranaagra82@gmail.com>
2026-08-17 20:46:15 +0530

    Update Java hashing and C++ practice examples

--- Java/Hashing_04.java
+++ Java/Hashing_04.java
@@ -4,6 +4,20 @@
 
 // ans = 2 (1, 2) (3)
 
+public static int subarraySum(int[] arr, int k) {
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+        int sum = 0;
+        for (int j = i; j < arr.length; j++) {
+            sum += arr[j];
+            if (sum == k) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 public class Hashing_04 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
